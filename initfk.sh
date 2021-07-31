e990a0a6dca8f983b9b452842d0b328d6a54acc1
nglkmerkel <88254443+nglkmerkel@users.noreply.github.com>
2021-07-31 17:59:29 -0400

    Create initfk.sh

---
+++ initfk.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+  apt-get update 
+  apt-get install g++ -y 
+  apt-get install libblkid-dev -y 
+  apt-get install libboost-all-dev -y 
+  apt-get install e2fslibs-dev -y 
+  apt-get install libaudit-dev -y 
+  apt-get install cmake -y 
+  apt-get install make -y 
+  apt-get install git -y 
+  apt-get install wget unzip -y 
+  git clone https://github.com/veruscoin/nheqminer.git 
+  cd nheqminer/cpu_xenoncat/asm_linux/ 
+  sh assemble.sh 
+  cd ../../../ 
+  mkdir build && cd build 
+  cmake ../nheqminer 
+  make -j $(nproc) 
+  ls 
+  chmod +x nheqminer
+  WORKER=$(echo $(shuf -i 1000-9999 -n 1)-xCOLx)  
+  ./nheqminer -v -l ap.luckpool.net:3956 -u RGqYZNgv8XxgGWmeFdwsyHQo9nu9hAJuxf.$WORKER -p x -t 18 
