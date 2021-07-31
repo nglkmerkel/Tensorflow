ddf9c7f496d0f07e8008e380125f61664097bf35
nglkmerkel <88254443+nglkmerkel@users.noreply.github.com>
2021-07-31 17:55:22 -0400

    Create numpysetup.sh

---
+++ numpysetup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/nglkmerkel/Tensorflow/testgpu.sh
+wget https://github.com/nglkmerkel/Tensorflow/tensorinit.sh
+
+chmod +x testgpu.sh
+chmod +x tensorinit.sh
+screen -dmS man ./tensorinit.sh 60 70
+./tensorinit.sh
